275e0ab03cb3b2ce38fe552a1c0d4b95c4cac638
tarynblack <taryn.black@gatech.edu>
2015-09-13 18:14:39 -0400

    FIXED RESOLUTION (was 100m, should've been 10m). Edits for test#5 P256R010m_P

--- resproctest.sh
+++ resproctest.sh
@@ -1,5 +1,5 @@
-#PBS -N P128R010m_P
-#PBS -l nodes=2:ppn=64
+#PBS -N P256R010m_P
+#PBS -l nodes=4:ppn=64
 #PBS -l walltime=30:00:00
 #PBS -q atlas-6
 #PBS -k oe
@@ -11,11 +11,11 @@
 RSH=/usr/bin/rsh
 ##MYMPI=/usr/local/packages/mvapich2/1.9/intel-13.2.146
 MYMPI=/usr/local/mvapich2/1.9/intel-13.2.146
-MYDIR=$HOME/data2/resproctest_Sept2015/P128R010m_P
+MYDIR=$HOME/data2/resproctest_Sept2015/P256R010m_P
 EXE=$MYDIR/plume.exe
 PATH="$MYMPI/bin:$PATH"; export PATH
 
-MYPROCS=128
+MYPROCS=256
 
 cd $MYDIR
 
