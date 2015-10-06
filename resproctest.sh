8316b040456942e4af86714d60a8f23011587b97
tarynblack <taryn.black@gatech.edu>
2015-10-06 14:00:06 -0400

    Turned on LES in mfix.dat and added L_SCALE0. Increased NLOG (at end of mfix.dat) to reduce rate of write to LAB.* files because they were blowing up and hogging memory. Restarting resproctests from this point with reduced parameter space - only 50m and 100m res for now.

--- resproctest.sh
+++ resproctest.sh
@@ -1,6 +1,6 @@
-#PBS -N P256R200m_S
-#PBS -l nodes=4:ppn=64
-#PBS -l walltime=30:00:00
+#PBS -N P064R050m_P
+#PBS -l nodes=1:ppn=64
+#PBS -l walltime=120:00:00
 #PBS -q atlas-6
 #PBS -k oe
 #PBS -j oe
@@ -11,11 +11,11 @@
 RSH=/usr/bin/rsh
 ##MYMPI=/usr/local/packages/mvapich2/1.9/intel-13.2.146
 MYMPI=/usr/local/mvapich2/1.9/intel-13.2.146
-MYDIR=$HOME/data2/resproctest_Sept2015/P256R200m_S
+MYDIR=$HOME/data2/resproctest_Sept2015/P064R050m_P
 EXE=$MYDIR/plume.exe
 PATH="$MYMPI/bin:$PATH"; export PATH
 
-MYPROCS=256
+MYPROCS=64
 
 cd $MYDIR
 
