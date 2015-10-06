3d16e9079e8f5776b9fa55b08aa8ca97abc4bfc5
tarynblack <taryn.black@gatech.edu>
2015-10-06 18:14:43 -0400

    Updated edits for P256R050m_S

--- resproctest.sh
+++ resproctest.sh
@@ -1,4 +1,4 @@
-#PBS -N P256R050m_P
+#PBS -N P256R050m_S
 #PBS -l nodes=4:ppn=64
 #PBS -l walltime=120:00:00
 #PBS -q atlas-6
@@ -11,7 +11,7 @@
 RSH=/usr/bin/rsh
 ##MYMPI=/usr/local/packages/mvapich2/1.9/intel-13.2.146
 MYMPI=/usr/local/mvapich2/1.9/intel-13.2.146
-MYDIR=$HOME/data2/resproctest_Sept2015/P256R050m_P
+MYDIR=$HOME/data2/resproctest_Sept2015/P256R050m_S
 EXE=$MYDIR/plume.exe
 PATH="$MYMPI/bin:$PATH"; export PATH
 
