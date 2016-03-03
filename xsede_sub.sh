4f660c18a8bd43e6af83fb318bca68d0075fa99d
Taryn Black <tg831617@login2.stampede.tacc.utexas.edu>
2016-03-03 12:25:57 -0600

    Increased submission time to allow 300s simulation

--- xsede_sub.sh
+++ xsede_sub.sh
@@ -4,6 +4,6 @@
 #SBATCH -n 64			# total number of mpi tasks requested
 #SBATCH -N 4			# Total number of nodes requested (16 cores/node)
 #SBATCH -p normal		# queue (partition) -- normal, development, etc.
-#SBATCH -t 4:00:00		# run time (hh:mm:ss) - 1.0 hours
+#SBATCH -t 30:00:00		# run time (hh:mm:ss) - 1.0 hours
 
 ibrun ./plume.exe		# run the MPI executable named a.out
