a10fe240d970411474f0c27a6e4c65dfda32ca20
tarynblack <taryn.black@gatech.edu>
2016-03-23 18:21:37 -0400

    Added new submission script clearly for atlas (vs xsede)

---
+++ atlas_sub.sh
@@ -0,0 +1,36 @@
+#PBS -N atlas_sub
+#PBS -l nodes=1:ppn=64
+#PBS -l walltime=100:00:00
+#PBS -q atlas-6
+#PBS -k oe
+#PBS -j oe
+#PBS -m abe
+##PBS -l mem=240gb
+
+##ulimit -s unlimited
+RSH=/usr/bin/rsh
+##MYMPI=/usr/local/packages/mvapich2/1.9/intel-13.2.146
+MYMPI=/usr/local/mvapich2/1.9/intel-13.2.146
+MYDIR=$HOME/data2/S_995_999
+EXE=$MYDIR/plume.exe
+PATH="$MYMPI/bin:$PATH"; export PATH
+
+MYPROCS=64
+
+cd $MYDIR
+
+echo "Started on `/bin/hostname`"
+echo
+echo "PATH is [$PATH]"
+echo
+echo "Nodes chosen are:"
+cat $PBS_NODEFILE
+echo
+
+##$EXE
+##mpirun -np $MYPROCS $EXE
+mpirun -rmk pbs $EXE
+
+##echo "======================================================================="
+##$MYMPI/bin/mpirun_rsh -rsh -np $MYPROCS -hostfile $PBS_NODEFILE $EXE
+##echo "======================================================================="
