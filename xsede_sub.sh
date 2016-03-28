cc76e34038cba242546dfd36784a9465fbd37b12
Taryn Black <tg831617@login2.stampede.tacc.utexas.edu>
2016-03-28 12:32:03 -0500

    Upped xsede walltime to be safe

--- xsede_sub.sh
+++ xsede_sub.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -J F_01_995_999		# job name
-#SBATCH -o F_01_995_999.out%j	# output and error file name (%j expands to jobID)
+#SBATCH -J F_1P_995_999		# job name
+#SBATCH -o F_1P_995_999.out%j	# output and error file name (%j expands to jobID)
 #SBATCH -n 64			# total number of mpi tasks requested
 #SBATCH -N 4			# Total number of nodes requested (16 cores/node)
 #SBATCH -p normal		# queue (partition) -- normal, development, etc.
-#SBATCH -t 30:00:00		# run time (hh:mm:ss) - 1.0 hours
+#SBATCH -t 33:00:00		# run time (hh:mm:ss) - 1.0 hours
 
 ibrun ./plume.exe		# run the MPI executable named a.out
